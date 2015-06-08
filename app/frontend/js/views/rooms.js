5f037fbcc6cac52a1eb046e0e04cc209d2aa3206
Stefan Laimer <stlaimer@gmail.com>
2015-06-08 13:47:34 +0200

    template for room items

--- app/frontend/js/views/rooms.js
+++ app/frontend/js/views/rooms.js
@@ -5,16 +5,18 @@ let Backbone 	= require('backbone'),
 class RoomsView extends Backbone.View {
 
   initialize () {
-  	this.$el = $('#app')
+  	this.$el 			= $('#app')
+  	this.list			= '#rooms'
   	this.template = _.template($('script[name="rooms"]').html())
+  	this.itemTem	= _.template($('script[name="rooms-item"]').html())
   	this.collection.bind('reset', console.log)
   }
 
   render () {
-  	this.$el.html('')
+  	this.$el.html(this.template)
     this.collection.each((model) => {
-      let profileTemplate = this.template(model.toJSON())
-      this.$el.append(profileTemplate);
+      let itemTemplate = this.itemTem(model.toJSON())
+      this.$el.find(this.list).append(itemTemplate);
     }, this)
     return this
   }
